cb838537c609f79bd0d6bc7f72abb36585b20fc0
Rajiv Mounguengue <rajiv.mounguengue@functionalstack.io>
2021-02-13 11:10:29 +0100

    chore: wip

---
+++ src/FsUssd/UssdRequest.fs
@@ -0,0 +1,23 @@
+namespace FsUssd
+
+type UssdRequestType =
+    | Initiation
+    | Response
+    | Release
+    | Timeout
+
+module UssdRequestType =
+
+    let create (value: string) =
+        match value.ToLower().Trim() with
+        | "initiation" -> UssdRequestType.Initiation
+        | "response" -> UssdRequestType.Response
+        | "release" -> UssdRequestType.Release
+        | _ -> UssdRequestType.Timeout
+
+type UssdRequest =
+    { Msisdn: string
+      SessionId: string
+      ServiceCode: string
+      Type: string
+      Input: string }
